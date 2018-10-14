12e7788c4d8de62419288741a87bfe0244358831
kgpavan <kgpavan.iiit@gmail.com>
2018-10-14 13:28:59 +0100

    question 2

--- question2.R
+++ question2.R
@@ -7,8 +7,7 @@ download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg'
               , mode='wb' )
 
 # Read the image
-picture <- jpeg::readJPEG('jeff.jpg'
-                          , native=TRUE)
+picture <- jpeg::readJPEG('jeff.jpg', native=TRUE)
 
 # Get Sample Quantiles corressponding to given prob
 quantile(picture, probs = c(0.3, 0.8) )
